8d690f2185010e49da6a426ad4278cb615d69aa1
Boombostak <smokejaguar3050@hotmail.com>
2018-02-08 15:07:05 -0700

    Fixed name entry click bug WOOOHOOO

--- Proto/Assets/_scripts/NameEntryUI.cs
+++ Proto/Assets/_scripts/NameEntryUI.cs
@@ -1,19 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class NameEntryUI : MonoBehaviour {
 
 	public InputField inputField;
+	public Canvas canvas;
 
 	// Use this for initialization
 	void Start () {
 		inputField = this.GetComponentInChildren<InputField> ();
 		inputField.Select ();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void Update(){
+		if (inputField.isFocused == false) {
+			inputField.Select ();
+		}
+		if (Input.GetKeyDown ("return")) {
+			canvas.enabled = false;
+		}
 	}
 }
